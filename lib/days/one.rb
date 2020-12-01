e16c8882c389f500ada1da74f03501b9bdbde9b8
Kelly Heitz <kelly.heitz@drip.com>
2020-12-01 08:31:49 -0600

    nested loops solution day 1

--- lib/days/one.rb
+++ lib/days/one.rb
@@ -1,20 +1,41 @@
 class One
-    def solve_part(part)
-        case part
-        when 1
-            part_one
-        when 2
-            part_two
-        else
-            "Error: requested part does not exist"
-        end
-    end
-
     def part_one
-        123
+        multiply_values_equaling_2020
     end
 
     def part_two
-        456
+        multiply_three_values_equaling_2020
+    end
+
+    def parse_inputs
+        return File::readlines('./lib/inputs/one.txt').map(&:chomp).map(&:to_i)
+    end
+
+    def multiply_values_equaling_2020
+        parse_inputs.each_with_index do |value, i|
+            parse_inputs.each_with_index do |other, j|
+                next if i == j
+                if value + other == 2020
+                    return value * other
+                end
+            end
+        end
+        puts "No correct answer found..."
+        return 0
+    end
+
+    def multiply_three_values_equaling_2020
+        parse_inputs.each_with_index do |value, i|
+            parse_inputs.each_with_index do |other, j|
+                parse_inputs.each_with_index do |third, k|
+                    next if i == j || i == k || j == k
+                    if value + other + third == 2020
+                        return value * other * third
+                    end
+                end
+            end
+        end
+        puts "No correct answer found..."
+        return 0
     end
 end
