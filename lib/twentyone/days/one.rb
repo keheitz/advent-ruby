e031f1c9426eabd38649dd500595255feffa5404
Kelly Heitz <kelly.heitz@drip.com>
2021-12-02 09:36:24 -0600

    2021 day one solutions

--- lib/twentyone/days/one.rb
+++ lib/twentyone/days/one.rb
@@ -3,15 +3,25 @@
 module TwentyOne
   class One
     def part_one
-      'one'
+      consecutive_inc_count(parse_inputs)
     end
 
     def part_two
-      '2'
+      consecutive_inc_count(three_measurement_sliding_window(parse_inputs))
+    end
+
+    private
+
+    def consecutive_inc_count(depths)
+      depths.each_cons(2).map { |a, b| b - a }.count(&:positive?)
+    end
+
+    def three_measurement_sliding_window(inputs)
+      inputs.each_cons(3).map { |a, b, c| a + b + c }
     end
 
     def parse_inputs
-      File.readlines('./lib/twenty/inputs/one.txt').map(&:chomp).map(&:to_i)
+      File.readlines('./lib/twentyone/inputs/one.txt').map(&:chomp).map(&:to_i)
     end
   end
 end
